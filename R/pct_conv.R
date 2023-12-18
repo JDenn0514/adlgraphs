ed90da89d9a4cb944f32899c24a4d4592d894c13
Jacob Dennen <80338305+JDenn0514@users.noreply.github.com>
2023-12-18 09:17:04 -0500

    Creating the pct_conv variable to help with frequency plots

---
+++ R/pct_conv.R
@@ -0,0 +1,16 @@
+#' Make proper percent labels
+
+#' This function is a wrapper around a very common data transformation that is
+#' done every time we make a frequency plot
+#'
+#' @param data A data frame or vector. Can be left blank if used during piping
+#' @param var a variable we want to convert to a percentage. The value is `pct`
+#' by default and should always be pct.
+
+pct_conv <- function(data, var = pct) {
+  data %>%
+    dplyr::mutate(
+      pct := round({{ var }} * 100, 1),
+      pct_lab := scales::percent(pct, scale = 1)
+    )
+}
