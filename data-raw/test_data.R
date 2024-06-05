8f359e12d782165a7d7d1c002e5f46c44ef97fa9
Jacob Dennen <80338305+JDenn0514@users.noreply.github.com>
2024-06-05 11:00:25 -0400

    Updating the data

--- data-raw/test_data.R
+++ data-raw/test_data.R
@@ -1,6 +1,16 @@
+test_data <- pol_pos[sample(nrow(pol_pos), 250), ]
+
+test_data <- test_data %>%
+  select(wts, edu, edu_f2, pid_f3, top:big_events, accept_hamas:dislike_jews, stick_together:wall_street, acts_avg, sdo_avg, trad_n)
+
+yar <- labelled::lookfor(test_data)
+
+haven::write_sav(test_data, here::here("data-raw", "test_data.sav"))
+
+rm(test_data)
+
 # import data set
 test_data <- haven::read_sav("data-raw/test_data.sav") %>%
-  dplyr::rename(edu = edu_f) %>%
   dplyr::mutate(
     edu_f = case_match_fct(
       edu,
@@ -8,8 +18,15 @@ test_data <- haven::read_sav("data-raw/test_data.sav") %>%
       3 ~ "Some College",
       4 ~ "Bachelor's Degree",
       5 ~ "Graduate Degree"
-    )
+    ),
+    edu_f2 = haven::as_factor(edu_f2),
+    resp_id = dplyr::row_number() %>%
+      structure(
+        label = "Unique ID for each Respondent",
+        note = "Just the"
+      )
   ) %>%
-  labelled::set_variable_labels(wts = "Survey weighting variable")
+  labelled::set_variable_labels(wts = "Survey weighting variable") %>%
+  relocate(resp_id)
 
 usethis::use_data(test_data, overwrite = TRUE)
