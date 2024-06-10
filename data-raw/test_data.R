349b2e0a7dd50b202feb54da33ecdb05ab0f1dde
Jacob Dennen <80338305+JDenn0514@users.noreply.github.com>
2024-06-09 21:50:25 -0400

    Updating the data set.

--- data-raw/test_data.R
+++ data-raw/test_data.R
@@ -3,8 +3,6 @@ test_data <- pol_pos[sample(nrow(pol_pos), 250), ]
 test_data <- test_data %>%
   select(wts, edu, edu_f2, pid_f3, top:big_events, accept_hamas:dislike_jews, stick_together:wall_street, acts_avg, sdo_avg, trad_n)
 
-yar <- labelled::lookfor(test_data)
-
 haven::write_sav(test_data, here::here("data-raw", "test_data.sav"))
 
 rm(test_data)
@@ -19,14 +17,15 @@ test_data <- haven::read_sav("data-raw/test_data.sav") %>%
       4 ~ "Bachelor's Degree",
       5 ~ "Graduate Degree"
     ),
-    edu_f2 = haven::as_factor(edu_f2),
+    edu_f2 = haven::as_factor(edu_f2) %>%
+      structure(transformation = ""),
     resp_id = dplyr::row_number() %>%
       structure(
         label = "Unique ID for each Respondent",
-        note = "Just the"
+        note = "Just the row"
       )
   ) %>%
   labelled::set_variable_labels(wts = "Survey weighting variable") %>%
-  relocate(resp_id)
+  dplyr::relocate(resp_id)
 
 usethis::use_data(test_data, overwrite = TRUE)
