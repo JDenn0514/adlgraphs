3cb409c37ed73961cfebf15aa98dd98769b73c18
Jacob Dennen <80338305+JDenn0514@users.noreply.github.com>
2024-11-26 06:57:39 -0500

    Updating teh get_means function

--- R/get_means.R
+++ R/get_means.R
@@ -5,7 +5,7 @@
 #'
 #' The `x`, `group`, and `wt` arguments can either be strings or symbols
 #' (meaning they can have quotes or no quotes). The benefit of this is that it
-#' makes it really easy to iterate this function over a list or vector of
+#' makes it easy to iterate this function over a list or vector of
 #' variables with other functions like [map()] [purrr::map()] or [walk()]
 #' [purrr::walk()] that are found in the `purrr` package.
 #'
@@ -13,11 +13,17 @@
 #'   function, this is not required.
 #' @param x Either a character string or symbol. The variable with which you want
 #'   to get the mean.
-#' @param group Either a character string or a symbol. The variable you want the
-#'   means to be grouped by.
+#' @param group <[`tidy-select`][dplyr_tidy_select]> A selection of columns to 
+#'   group the data by in addition to `treats`. This operates very similarly
+#'   to `.by` from dplyr (for more info on that see [?dplyr_by][dplyr_by]). It can
+#'   also be a character vector, but it can't be an external vector.
 #' @param wt Weights. Add if you have a weighting variable and want to get
-#'   weighted means
-#' @param decimals Number of decimals to round the results to. Default is 2.
+#'   weighted means.
+#' @param decimals Number of decimals to round the results to. Default is 3.
+#' @param na.rm Logical. Determines if NAs should be removed from the grouping
+#'   variables prior to analysis. Default is TRUE.
+#' @param conf_level What should the confidence level be when calculating
+#'   confidence intervals. Defaults to 0.95 
 #'
 #' @examples
 #' # load the package
@@ -49,193 +55,146 @@
 #'   mutate(top_f2 = make_dicho(top)) %>%
 #'   get_means(trad_n, top_f2, wts)
 #'
-#'
-#'
-#'
 #' @export
-#'
-#'
+get_means <- function(
+  data, 
+  x, 
+  group = NULL, 
+  wt = NULL,
+  decimals = 3, 
+  na.rm = TRUE,
+  conf_level = 0.95
+) {
+  x_name <- deparse(substitute(x))
+  # Ensure x is a string
+  x <- rlang::as_name(rlang::ensym(x))
 
-get_means <- function(data, x, group, wt, decimals = 2) {
-
-  # get the object's name
-  x_lab <- deparse(substitute(x))
-
-  # use enexpr() to capture the expressions supplied in "x"
-  # enexpr returns a naked expression of the argument supplied in "x"
-  # this is what allows the input to be either a string or a symbol
-  x <- rlang::enexpr(x)
-
-  if (!is.character(x)) {
-    # if the name supplied in "x" is not a character...
-
-    # capture x and convert to a symbol object with ensym()
-    #then use as_name() to make it a string
-    x <- rlang::as_name(rlang::ensym(x))
-
-  }
-
+  # Check if x is numeric
   if (!is.numeric(data[[x]])) {
-    # if the variable x is not numeric return an error
-
-    cli::cli_abort(
-      c(
-        "`{x_lab}` must be a vector of class {.cls numeric}",
-        x = "You've supplied a {.cls {class(x)}} vector"
-      )
-    )
+    cli::cli_abort(c(
+      "`{x}` must be a numeric variable.",
+      x = "Supplied variable is {class(data[[x]])}."
+    ))
   }
 
-
-  if (!missing(group)) {
-    # if group is not missing
-
-    # use enexpr() to capture the expressions supplied in "group"
-    # enexpr returns a naked expression of the argument supplied in "group"
-    # this is what allows the input to be either a string or a symbol
-    group <- rlang::enexpr(group)
-
-    if (!is.character(group)) {
-      # if the object supplied in "group" is not a character...
-
-      # capture group and convert to a symbol object with ensym()
-      #then use as_name() to make it a string
-      group <- rlang::as_name(rlang::ensym(group))
-
-    }
-
-    if (is.numeric(data[[group]]) && !is.null(attr_val_labels(data[[group]]))) {
-      # if group is class numeric AND DOES contain value labels
-
-      # convert to a factor with sjlabelled
-      data <- data %>%
-        # convert to a factor using make_factor
-        dplyr::mutate(group_f = make_factor(.data[[group]])) %>%
-        # group by group_f
-        dplyr::group_by(group_f)
-
-    } else if (is.character(data[[group]]) || is.factor(data[[group]])) {
-      # if group is of class character or factor return x
-
-      data <- data %>%
-        # just make a new variable called group_f comprised of gropu
-        dplyr::mutate(group_f = .data[[group]]) %>%
-        # group by group_f
-        dplyr::group_by(group_f)
-
-
-    } else {
-      # if group is anything else (ie numeric)
-
-      data <- data %>%
-        # force to a factor
-        dplyr::mutate(group_f = as.factor(.data[[group]])) %>%
-        # group by group_f
-        dplyr::group_by(group_f)
-
-    }
-
-  }
-
+  # Prepare group variables
+  # if the data is grouped, use dplyr::group_vars to get them, else set to NULL
+  group_names <- if(inherits(data, "grouped_df")) dplyr::group_vars(data) else NULL
+  # if group arg is missing set to NULL, else use select_groups to capture it
+  group_vars <- if (missing(group)) NULL else select_groups({{ group }}, data)
+  # group_vars <- if (missing(group)) NULL else eval_select_by(rlang::enexpr(group), data)
+  # remove the "c" from the group_vars vector if it is there
+  group_vars <- group_vars[group_vars != "c"]
+  # get the groups
+  group_names <- unique(c(group_names, group_vars))
+  
+  # Prepare weights
   if (missing(wt)) {
-    # if missing wt
-
-    # calculate the mean with CIs, standard deviation, standard error and N
-    data_mean <- data %>%
-      dplyr::summarise(
-        # calculate the mean
-        mean = mean(.data[[x]],  na.rm = TRUE),
-        # calculate the standard deviation
-        sd = sd(.data[[x]], na.rm = TRUE),
-        # get the number of respondents
-        n = dplyr::n()
-      ) %>%
-      dplyr::mutate(
-        # calculate the standard error
-        std.error = sd/sqrt(n),
-        # calculate the lower CI
-        conf.low = mean - qt(1 - ((1 - 0.95) / 2),  n - 1) * std.error,
-        # calculate the higher CI
-        conf.high = mean + qt(1 - ((1 - 0.95) / 2),  n - 1) * std.error,
-        # round all of the numbers
-        dplyr::across(dplyr::where(is.numeric), ~round(.x, decimals))
-      ) %>%
-      dplyr::select(-std.error)
-
-    if (!missing(group)) {
-      # if not missing group
-
-      # rename the group_f variable with the name supplied in "group"
-      data_mean <- data_mean %>% dplyr::rename({{ group }} := group_f)
-      # give data_mean as the output
-      return(data_mean)
-
-    } else {
-      # if group is missing
-
-      # return the original data_mean as the output
-      return(data_mean)
-
-    }
-
+    wt <- "wts"
+    data[[wt]] <- rep(1, length(data[[x]]))  
   } else {
-    # if wt is not missing
-
-    # use enexpr() to capture the expressions supplied in "wt"
-    # enexpr returns a naked expression of the argument supplied in "wt"
-    # this is what allows the input to be either a string or a symbol
-    wt <- rlang::enexpr(wt)
-
-    if (!is.character(wt)) {
-      # if the object supplied in "x" is not a character...
-
-      # capture x and convert to a symbol object with ensym()
-      #then use as_name() to make it a string
-      wt <- rlang::as_name(rlang::ensym(wt))
-
-    }
-
-
-    data_mean <- data %>%
-      dplyr::summarise(
-        # calculate the mean
-        mean = weighted.mean(.data[[x]], .data[[wt]], na.rm = TRUE),
-        # calculate the standard deviation
-        sd = sd(.data[[x]], na.rm = TRUE),
-        # get the number of respondents
-        n = dplyr::n()
-      ) %>%
-      dplyr::mutate(
-        # calculate the standard error
-        std.error = sd/sqrt(n),
-        # calculate the lower CI
-        conf.low = mean - qt(1 - ((1 - 0.95) / 2),  n - 1) * std.error,
-        # calculate the higher CI
-        conf.high = mean + qt(1 - ((1 - 0.95) / 2),  n - 1) * std.error,
-        # round all of the numbers to the second decimal
-        dplyr::across(dplyr::where(is.numeric), ~round(.x, decimals))
-      ) %>%
-      dplyr::select(-std.error)
-
-    if (!missing(group)) {
-      # if not missing group
-
-      # rename the group_f variable with the name supplied in "group"
-      data_mean <- data_mean %>% dplyr::rename({{ group }} := group_f)
-      # give data_mean as the output
-      return(data_mean)
-
-    } else {
-      # if group is missing
-
-      # return the original data_mean as the output
-      return(data_mean)
-
-    }
-
+    # ensure that string or symbol are accepted in wt
+    wt <- rlang::as_name(rlang::ensym(wt))
+    data[[wt]][is.na(data[[wt]])] <- 0
   }
 
+  # get the data
+  data <- data[c(x, group_names, wt)]
+  
+  # if na.rm is TRUE remove NAs
+  if (na.rm) data <- data[stats::complete.cases(data),]  
+
+  if (!is.null(group_names)) {
+    # if the group arg is not missing, apply grouping based on group_names
+    data <- data %>% 
+      dplyr::group_by(dplyr::across(tidyselect::all_of(group_names)))
+  } 
+  
+  # Summarize data
+  out <- data %>%
+    dplyr::summarise(
+      # calculate the weighted n
+      n = sum(.data[[wt]], na.rm = TRUE), 
+      # calculate the mean (weighted sum / n)
+      mean = sum(.data[[x]] * .data[[wt]], na.rm = TRUE) / n,
+      # calculate the weighted sd
+      sd = sqrt(sum(.data[[wt]] * (.data[[x]] - mean)^2, na.rm = TRUE) / n),
+      # remove the groups
+      .groups = "drop"
+    ) %>% 
+    dplyr::mutate(
+      # calculate std.error
+      std.error = sd / sqrt(n),
+      # calculate the confidence invtervals
+      conf.low = mean - qt(1 - ((1 - 0.95) / 2), n - 1) * std.error,
+      conf.high = mean + qt(1 - ((1 - 0.95) / 2), n - 1) * std.error,
+      # convert all group variables to a factor
+      dplyr::across(
+        # run the function over the variables in group_names
+        tidyselect::all_of(group_names),
+        # convert to a factor, removing levels, forcing to factor, and keeping NA as NA
+        ~ make_factor(.x, drop_levels = TRUE, force = TRUE, na.rm = TRUE)
+      ),
+      # round all numeric columns 
+      dplyr::across(
+        dplyr::where(is.numeric),
+        ~round(.x, decimals)
+      )
+    ) %>% 
+    # keep only relevant variables and reorder them
+    dplyr::select(c(tidyselect::all_of(group_names), mean, sd, n, conf.low, conf.high))
+  
+    if (!is.null(group_names)) {
+      # if there are groups add the value labels
+  
+      # get the variable labels as a named list
+      group_labels <- attr_var_label(data[,group_names])
+      # for each value in names(group_labels) add the variable label from group_labels
+      for (y in names(group_labels)) attr(out[[y]], "label") <- group_labels[[y]]
+  
+    }
+  
+    if (!is.null(attr_var_label(data[[x]]))) {
+      # if there is a variable label in the x variable
+  
+      # add the variable label of x as an attribute called 
+      # variable_label to the output dataframe
+      attr(out, "variable_label") <- attr_var_label(data[[x]])
+      # add the variable name of x as an attribute called
+      # variable_name to the output dataframe
+      attr(out, "variable_name") <- x
+  
+    } else {
+      # if x does not have a variable label
+  
+      # add the variable name of x as an attribute called
+      # variable_label to the output dataframe
+      attr(out, "variable_label") <- x_name
+      # add the variable name of x as an attribute called
+      # variable_name to the output dataframe    
+      attr(out, "variable_name") <- x_name
+  
+    }
+  
+    # add an attribute containing the names of the grouping variables
+    attr(out, "group_names") <- group_names
+  
+    # add a variable for the n variable
+    attr(out$n, "label") <- "N"
+    # add a variable label for the mean variable
+    attr(out$mean, "label") <- "Mean"
+    # add a variable label for the mean variable
+    attr(out$sd, "label") <- "SD"
+    # add a variable label for the mean variable
+    attr(out$conf.low, "label") <- "Low CI"
+    # add a variable label for the mean variable
+    attr(out$conf.high, "label") <- "High CI"
+  
+    # get the classes of the data.frame
+    class_names <- class(out)
+    # add adlgraphs_freqs to the classes
+    attr(out, "class") <- c("adlgraphs_means", class_names)
+  
+  out
 }
-
-
 
--- tests/testthat/test-make_factor.R
+++ tests/testthat/test-make_factor.R
@@ -2,35 +2,46 @@
 # Labelled values ---------------------------------------------------------
 
 #### NUMERIC VECTORS
-test_that("all labels are preserved", {
+testthat::test_that("all labels are preserved if drop_levels is FALSE", {
   s1 <- haven::labelled(rep(1, 3), c("A" = 1, "B" = 2, "C" = 3), label = "Variable label")
   exp <- factor(rep("A", 3), levels = c("A", "B", "C")) %>%
     structure(
       transformation = "Converted 's1' into a factor based on its value labels",
       label = "Variable label"
     )
-  expect_equal(make_factor(s1), exp)
+    testthat::expect_equal(make_factor(s1, drop_levels = FALSE), exp)
 })
 
-test_that("all labels are preserved when using a non-haven_labelled numeric vector", {
+testthat::test_that("Only labels in data are preserved if drop_levels is TRUE", {
+  s1 <- haven::labelled(rep(1, 3), c("A" = 1, "B" = 2, "C" = 3), label = "Variable label")
+  exp <- factor(rep("A", 3), levels = c("A")) %>%
+    structure(
+      transformation = "Converted 's1' into a factor based on its value labels",
+      label = "Variable label"
+    )
+  testthat::expect_equal(make_factor(s1, drop_levels = TRUE), exp)
+})
+
+
+testthat::test_that("all labels are preserved when using a non-haven_labelled numeric vector", {
   #values <- stats::setNames(c(0, 1), c("A", "B"))
   s1 <- structure(c(0, 1, 0, 1), labels = stats::setNames(c(0, 1), c("A", "B")))
   exp <- factor(c("A", "B", "A", "B"), levels = c("A", "B")) %>%
     structure(transformation = "Converted 's1' into a factor based on its value labels")
-  expect_equal(make_factor(s1), exp)
+  testthat::expect_equal(make_factor(s1), exp)
 })
 
 #### CHARACTER VECTORS
-test_that("character labelled converts to factor", {
+testthat::test_that("character labelled converts to factor", {
   s1 <- haven::labelled(c("M", "M", "F"), c(Male = "M", Female = "F"))
   exp <- factor(c("Male", "Male", "Female"), levels = c("Male", "Female")) %>%
     structure(
       transformation = "Converted 's1' into a factor based on its value labels"
     )
-  expect_equal(make_factor(s1), exp)
+    testthat::expect_equal(make_factor(s1), exp)
 })
 
-test_that("all labels are preserved when using a non-haven_labelled numeric vector", {
+testthat::test_that("all labels are preserved when using a non-haven_labelled numeric vector", {
   #values <- stats::setNames(c(0, 1), c("A", "B"))
   s1 <- structure(c("A", "B", "A", "B"), labels = stats::setNames(c("A", "B"), c("Letter A", "Letter B")))
   exp <- factor(c("Letter A", "Letter B", "Letter A", "Letter B"), levels = c("Letter A", "Letter B")) %>%
@@ -40,6 +51,11 @@ test_that("all labels are preserved when using a non-haven_labelled numeric vect
 
 test_that("Should return same value as s1", {
   s1 <- letters
+  expect_equal(make_factor(s1), as.factor(s1))
+})
+
+test_that("should force s1 to a factor", {
+  s1 <- c(0, 1, 2)
   expect_equal(make_factor(s1), as.factor(s1))
 })
 
@@ -59,10 +75,7 @@ test_that("return error when some values are missing labels- character vector", 
   expect_error(make_factor(s1), "Each value in `x` must have value labels")
 })
 
-test_that("return error when there aren't any value labels- numeric vector", {
-  s1 <- c(0, 1, 2)
-  expect_error(make_factor(s1), "The vector provided in `x` does not have value labels")
-})
+
 
 
 
