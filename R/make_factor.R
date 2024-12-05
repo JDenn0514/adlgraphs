6217bf87c199adfca35ce57d1c349d873b9bfc89
Jacob Dennen <80338305+JDenn0514@users.noreply.github.com>
2024-12-05 10:18:58 -0500

    Adding an additional argument to make_factor

--- R/make_factor.R
+++ R/make_factor.R
@@ -1,4 +1,7 @@
 #' Convert a labelled vector into a factor
+#' 
+#' #' @description
+#' `r lifecycle::badge("experimental")`
 #'
 #' `make_factor()` takes a labelled vector and converts it to a factor variable
 #' using the value labels. This works with numeric, character, and factor vectors.
@@ -22,8 +25,19 @@
 #' also enables more customization over the output of the labels. Another key
 #' difference between this function and those is that if there are values without
 #' labels, this function returns an error.
+#' 
+#' #' @section Lifecycle:
+#' The `levels` argument in `make_factor()` has not been fully developed yet. 
+#' Currently, it only allows you to specify the levels on a character or 
+#' factor vector without value labels. If you try using it for anything else
+#' it will not work. At some point, I would like to fully implement it though 
+#' so that you can specify the levels yourself, kind of like with `factor()`.
+#' 
 #'
 #' @param x A vector with value labels. Can be numeric, character, or a factor
+#' @param levels You can supply levels to be used if the vector is a character or 
+#'   factor. It is not clear how this will fit in with the rest of the function 
+#'   so you might get weird results at first.
 #' @param ordered Logical. Determines if the factor be ordered. Defaults to `TRUE.`
 #' @param drop_levels Logical. Determines if unused factor levels should be dropped.
 #'   Defaults to `TRUE.`
@@ -53,9 +67,9 @@
 
 
 
-make_factor <- function(x, ordered = FALSE, drop_levels = TRUE, force = TRUE, na.rm = FALSE) {
+make_factor <- function(x, levels = NULL, ordered = FALSE, drop_levels = TRUE, force = TRUE, na.rm = FALSE) {
   # get the variable's name as a string
-  x_lab <- deparse(substitute(x))
+  x_name <- deparse(substitute(x))
 
   # Get the variable label (assumes attr_var_label function exists)
   variable_label <- attr_var_label(x)
@@ -63,25 +77,44 @@ make_factor <- function(x, ordered = FALSE, drop_levels = TRUE, force = TRUE, na
   # Get the value labels (assumes attr_val_labels function exists)
   value_labels <- attr_val_labels(x)
 
-  # If no value labels, handle accordingly
   if (is.null(value_labels)) {
     # if there aren't any value labels:
 
     if (is.factor(x)) {
-      # Return x if it's already a factor
-      return(x)  
+
+      # if it is a character vector
+      if (!missing(levels) && !is.null(levels)) {
+        x <- factor(x, levels = levels)
+        lvls_names <- paste(levels, collapse = ", ")
+        attr(x, "transformation") <- paste("Updated '", x_name, "' to have levels: '", lvls_names, "'", sep = "")
+        lvls_names <- paste(levels, collapse = ", ")
+        return(x)
+      } else {
+        return(x)
+      } 
+ 
     } else if (is.character(x)) {
-      # if x is a character vector convert to a factor
-      return(factor(x)) 
+
+      # if it is a character vector
+      if (!missing(levels) && !is.null(levels)) {
+        x <- factor(x, levels = levels)
+        lvls_names <- paste(levels, collapse = ", ")
+        attr(x, "transformation") <- paste("Converted '", x_name, "' into a factor with levels: '", lvls_names, "'", sep = "")
+        return(x)
+      } else {
+        # if levels is missing
+        return(factor(x))
+      }
+      
     } else if (is.numeric(x) && isTRUE(force)) {
       # if x is numeric and force = TRUE, force to a factor and return warning
       warning("`x` has no value labels so forcing to a factor with `as.factor()`")
       return(as.factor(x))
     } else if (is.numeric(x) && isFALSE(force)) {
       # if x is numeric and force = false, return an error 
-      stop(
-        "The vector provided in `x` does not have value labels.
-        If you want to force it to a factor, set `force = TRUE`."
+      cli::cli_abort(
+        "The vector provided in `x` does not have value labels.",
+        "i" = "If you want to force it to a factor, set `force = TRUE`."
       )
     }
   }
@@ -128,12 +161,15 @@ make_factor <- function(x, ordered = FALSE, drop_levels = TRUE, force = TRUE, na
   x <- factor(x, levels = x_levels, ordered = ordered)
 
 
-  # If a variable label exists, preserve it in the factor
+  
   if (!is.null(variable_label)) {
+    # If a variable label exists, preserve it in the factor
     attr(x, "label") <- variable_label
-    attr(x, "transformation") <- paste("Converted '", x_lab, "' into a factor based on its value labels", sep = "")
+    # add the transformation attribute
+    attr(x, "transformation") <- paste("Converted '", x_name, "' into a factor based on its value labels", sep = "")
   } else {
-    attr(x, "transformation") <- paste("Converted '", x_lab, "' into a factor based on its value labels", sep = "")
+    # add the transformation attribute
+    attr(x, "transformation") <- paste("Converted '", x_name, "' into a factor based on its value labels", sep = "")
   }
 
   return(x)
