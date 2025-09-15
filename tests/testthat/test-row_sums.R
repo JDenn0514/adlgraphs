7cec7b627cf176ad4b7f2344d965ce3f9acc8732
Jacob Dennen <80338305+JDenn0514@users.noreply.github.com>
2025-09-15 15:05:32 -0400

    Adding the test for row_sums

---
+++ tests/testthat/test-row_sums.R
@@ -0,0 +1,43 @@
+testthat::test_that("row_means is equal to rowMeans", {
+
+  test <- test_data %>% 
+    dplyr::mutate(
+      new = row_sums(
+        cols = c(top_rev:deserving_flip),
+        label = "Social Dominance Orientation Sums",
+        na.rm = TRUE
+      ),
+      base = rowSums(dplyr::pick(c(top_rev:deserving_flip)), na.rm = TRUE) %>% 
+        structure(
+          label = "Social Dominance Orientation Sums",
+          variables = c("top_rev", "inferior_rev", "dominate_flip", "deserving_flip"),
+          transformation = "Took the sum of top_rev, inferior_rev, dominate_flip, deserving_flip"
+        )
+    )
+  
+  testthat::expect_equal(test$new, test$base)
+
+})
+
+testthat::test_that("row_means is equal to rowMeans without label", {
+
+  test <- test_data %>% 
+    dplyr::mutate(
+      new = row_sums(
+        cols = c(top_rev:deserving_flip),
+        na.rm = TRUE
+      ),
+      base = rowSums(dplyr::pick(c(top_rev:deserving_flip)), na.rm = TRUE) %>% 
+        structure(
+          variables = c("top_rev", "inferior_rev", "dominate_flip", "deserving_flip"),
+          transformation = "Took the sum of top_rev, inferior_rev, dominate_flip, deserving_flip"
+        )
+    )
+  
+  testthat::expect_equal(test$new, test$base)
+
+})
+
+
+
+
